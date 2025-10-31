737815b44e045d9114a8311a7cc33d5cf2b8ae24
Go0ners <g@g.Red>
2025-10-31 15:52:58 -0400

    chore: remove test file

--- test_delete_folder.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from pathlib import Path
-import sys
-sys.path.insert(0, '.')
-from orphan_sweeper import OrphanSweeper
-
-sweeper = OrphanSweeper()
-file_path = Path("test_folder/Movie.2024/Movie.2024.mkv")
-sweeper.delete_file(file_path, dry_run=False)
